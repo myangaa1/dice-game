0c555d34accba064fd2f2721e81751afc143c1a1
Myangaa <m.myanganbayar@gmail.com>
2019-12-12 12:29:18 +0700

    Shoog shiddeg bolgox

--- app.js
+++ app.js
@@ -6,17 +6,25 @@ var scores = [0, 0];
 
 // Тоглогчийн ээлж дээр цуглуулсан оноог хадгалах хувьсагч
 var roundScore = 0;
-
 // Шооны аль талаараа буусныг хадгалаx хувьсагч (1-6 хүртэлх тоог энэ хувьсагчид санамсаргүйгээр олгож өгнө)
-var dice = Math.floor(Math.random() * 6) + 1;
 
 // Программ эхлэхэд бэлдэх
-document.querySelector("#score-0").textContent = 0;
-document.querySelector("#score-1").textContent = 0;
+// document.querySelector("#score-0").textContent = 0;
+document.getElementById("score-0").textContent = "0";
+document.getElementById("score-1").textContent = "0";
+document.getElementById("current-0").textContent = "0";
+document.getElementById("current-1").textContent = "0";
 
-document.querySelector("#current-0").textContent = 0;
-document.querySelector("#current-1").textContent = 0;
+var diceDom = document.querySelector(".dice");
+diceDom.style.display = "none";
 
-document.querySelector(".dice").style.display = "none";
+document.querySelector(".btn-roll").addEventListener("click", function() {
+  var diceNumber = Math.floor(Math.random() * 6) + 1;
+  diceDom.style.display = "block";
+  diceDom.src = "dice-" + diceNumber + ".png";
+  // ------------------------------------------------------------------------------------
+  roundScore = roundScore + diceNumber;
+  if (diceNumber === 1) roundScore = 0;
 
-console.log("Shoo :" + dice);
+  document.getElementById("current-" + activePlayer).textContent = roundScore;
+});
